84fa373bd2dc6b33404f802e22003a91b4a42eb3
Abdul Hadi Khan Khokhar <hadikk0000@gmail.com>
2024-04-22 18:05:06 +0500

    Initial commit

---
+++ wc.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const yourSentence = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence: "
+});
+const words = yourSentence.sentence.trim().split(" ");
+console.log(words);
+console.log(`The words in your sentence are: ${words.length}`);
